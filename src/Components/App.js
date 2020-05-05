600ced68941f8785a2ccd4b5d63817ea73d766f6
anassohail99 <anas.sohailabs1@gmail.com>
2020-05-06 00:03:56 +0500

    App.js Version 1.0

---
+++ src/Components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import TaskList from "./TaskList";
+import TaskListContextProvider from "../Context/TaskListContext";
+import TaskForm from "./TaskForm";
+
+const App = () => {
+  return (
+    <TaskListContextProvider>
+      <div className="container">
+        <div className="app-wrapper">
+          <div className="main">
+            <TaskForm />
+            <TaskList />
+          </div>
+        </div>
+      </div>
+    </TaskListContextProvider>
+  );
+};
+
+export default App;
